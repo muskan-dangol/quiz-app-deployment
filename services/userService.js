5fd918c8fb01bd5e8efc14bf5c8a0ccf4b45daec
muskan-dangol <dangolmuskan19@gmail.com>
2024-11-20 16:58:47 +0200

    adding admin

--- services/userService.js
+++ services/userService.js
@@ -1,7 +1,8 @@
 import { sql } from "../database/database.js";
 
 const addUser = async (email, password) => {
-  await sql`INSERT INTO users (email, password) VALUES (${email}, ${password})`;
+  // await sql`INSERT INTO users (email, password) VALUES (${email}, ${password})`;
+  await sql`INSERT INTO users (email, password, admin) VALUES (${email}, ${password}, ${true})`;
 };
 
 const findUserByEmail = async (email) => {
